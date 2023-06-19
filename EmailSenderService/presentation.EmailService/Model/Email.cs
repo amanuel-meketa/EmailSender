4503784cc99d1e5ebe2bbbd4084fcec0a628203c
Amanuel <Amanuel.Eshete@melkamsoft.com>
2023-06-19 15:19:40 +0300

    Clean .NET 7 Layered Architecture Solution.

---
+++ EmailSenderService/presentation.EmailService/Model/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace data.EmailService.Model
+{
+    public class Email
+    {
+        public string? From { get; set; }
+        public string? To { get; set; }
+        public string? Message { get; set; }
+    }
+}
